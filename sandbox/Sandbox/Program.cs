04c0d3b1c5045e110c1292222e37928098251f4e
melissard02 <melissa.rd1258@gmail.com>
2025-04-24 16:14:48 -0600

    Fixed the name code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        //Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("Input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Your first name is: {firstName}");
+        Console.Write("Input your Last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
