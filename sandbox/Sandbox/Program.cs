878beb3894288117c37af7269f21f00b920fe04a
melissard02 <melissa.rd1258@gmail.com>
2025-05-01 11:15:36 -0600

    Class Sandbox Updated

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,15 +2,54 @@ using System;
 
 class Program
 {
+    static double AddNumbers(double a, double b)
+    {
+        return a + b;
+
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("Sup");
+        // Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Sup");
+        // HIGHLIGHT THE WHOLE THING AND TYPE CTR AND / AND IT AUTOMATICALLY COMMENTS IT
+
+        // for(int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine("Hello Bob");
+        // }
+
+        // int x = 0;
+        // int y= x++;
+        // Console.WriteLine(x);
+        // Console.WriteLine(y);
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(4);
+        // myNumbers.Add(99);
+        // myNumbers.Add(567);
+
+        // foreach(int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        double total = AddNumbers(123.98, 1235.76);
+        Console.WriteLine(total);
 
 
-        for(int i = 0; i < 20; i++)
+        bool done = false;
+        while(!done)
         {
-            Console.WriteLine("Hello Bob");
+            // ....
+            done = true;
         }
+
+        do
+        {
+
+
+        } while(!done);
     }
 }
