8e16d74b0fee2769c77a5ce42610be8f601f63b6
melissard02 <melissa.rd1258@gmail.com>
2025-05-15 15:55:57 -0600

    Journal Attempt #1 Complete

--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,11 +1,70 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Journal 
+class Journal
 {
+    //Makin a list to hold our entry objects
+    private List<Entry> entries = new List<Entry>();
+
+    public void AddEntry()
+    {
+        Entry newEntry = new Entry();
+        newEntry.CreateEntry();
+        entries.Add(newEntry);
+    }
 
 
 
 
+    //display all the journal entries so far to the terminal
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in entries) {
+            Console.WriteLine(entry);
+        }
 
-    
+    }
+
+    // //Create a function that saves the journal entries to a text file
+    public void SaveJournal(string filename)
+    {
+        using (StreamWriter transcribe = new StreamWriter(filename))
+        {
+            foreach (Entry entry in entries)
+            {
+                transcribe.WriteLine(entry);
+            }
+        }
+    }
+
+    // //Create a function that selects the text file that will be manipulated
+    public void LoadJournal(string filename)
+    {
+        if (File.Exists(filename))
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (reader.Peek() != -1)
+                {
+                    string line = reader.ReadLine();
+                    string[] parts = line.Split(" - ");
+
+
+                    Entry entry = new Entry();
+                    entry.Date = DateTime.Parse(parts[0]);
+                    entry.Prompt = parts[1];
+
+                    string response = reader.ReadLine();
+                    entry.Response = response;
+                    entries.Add(entry);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+
 }
--- prove/Develop02/menu.cs
+++ prove/Develop02/menu.cs
@@ -2,9 +2,49 @@ using System;
 
 class Menu
 {
+    public int DisplayMenu()
+    {
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display Journal");
+        Console.WriteLine("3. Save Journal to a File");
+        Console.WriteLine("4. Load Journal from a File");
+        Console.WriteLine("5. Exit");
+
+        string userChoice = Console.ReadLine();
+        int choice = int.Parse(userChoice);
+
+        return choice;
+
+    }
 
 
-
-
-    
+    public void CreateMenu(Journal journal)
+    {
+        int response = 0;
+        while (response != 5)
+        {
+            response = DisplayMenu();
+            switch (response)
+            {
+                case 1:
+                    journal.AddEntry();
+                    break;
+                case 2:
+                    journal.DisplayJournal();
+                    break;
+                case 3:
+                    Console.WriteLine("Enter a filename to save to: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveJournal(saveFile);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter a file to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadJournal(loadFile);
+                    break;
+                case 5:
+                    break;
+            }
+        }
+    }
 }
