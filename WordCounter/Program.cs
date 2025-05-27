95bf9d1d9238c4b1a1d72c9ff45aed0dad93af9e
melissard02 <melissa.rd1258@gmail.com>
2025-05-27 11:15:57 -0600

    Word Counter Project

---
+++ WordCounter/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello World");
+        WordCounter wordCounter = new WordCounter("This is a test sentence");
+        wordCounter.DisplayWords();
+    }
+
+}
