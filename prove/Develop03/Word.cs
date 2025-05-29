ee871d39d13aa4df497b02ac60236c2f5cf09bd9
melissard02 <melissa.rd1258@gmail.com>
2025-05-29 15:55:55 -0600

    Word Class functional

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,45 +1,46 @@
+// Word.cs
 using System;
-
 class Word
 {
     private string _word;
 
     private bool _hidden;
 
-
-    public string GetWord()
+    public Word(string word)
     {
-
-        return _word;
+        _word = word;
+        _hidden = false;
     }
 
     public bool IsHidden()
     {
-
         return _hidden;
     }
 
     public void SetIsHidden(bool hidden)
     {
-
+        _hidden = hidden;
     }
 
     public string GetWordString()
     {
-        return;
+        if (_hidden)
+        {
+            return new string('_', _word.Length);
+        }
+        else
+        {
+            return _word;
+        }
     }
 
     public void DisplayWord()
     {
-        return;
+        Console.Write(GetWordString() + ' ');
     }
 
     private int GetWordLength()
     {
-        return int;
+        return _word.Length;
     }
-
-
-
-
 }
