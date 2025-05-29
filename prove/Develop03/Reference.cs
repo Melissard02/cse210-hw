49c7fa81a7c0f8422dce164311a7b805fdddf006
melissard02 <melissa.rd1258@gmail.com>
2025-05-29 11:08:13 -0600

    Scripture Memorizer Classes Started

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Reference
+{
+    private string _bookname;
+
+    private int _chapter;
+
+    private int[] _verse;
+
+
+    public Reference(string name, int chapter, int verse)
+    {
+
+    }
+
+    public Reference(string name, int chapter, int startVerse, int endVerse)
+    {
+
+    }
+
+    public void ShowReference()
+    {
+
+    }
+
+    public string GetReference()
+    {
+        return;
+    }
+
+    private string GetReferenceString()
+    {
+        return;
+    }
+
+
+
+
+
+
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Word
+{
+    private string _word;
+
+    private bool _hidden;
+
+
+    public string GetWord()
+    {
+
+        return _word;
+    }
+
+    public bool IsHidden()
+    {
+
+        return _hidden;
+    }
+
+    public void SetIsHidden(bool hidden)
+    {
+
+    }
+
+    public string GetWordString()
+    {
+        return;
+    }
+
+    public void DisplayWord()
+    {
+        return;
+    }
+
+    private int GetWordLength()
+    {
+        return int;
+    }
+
+
+
+
+}
