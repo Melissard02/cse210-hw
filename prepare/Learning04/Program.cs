678b0c5dc8098fe901f089ff48e463c157a1ceba
melissard02 <melissa.rd1258@gmail.com>
2025-06-05 12:10:16 -0600

    Assignments Inheritance Practice

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Grant", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Janey Smith", "Addition", "7.2", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Dain Brooks", "World History", "The Cause of the Brazilian Revolution");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
