7dff86ca4ba9c52e5d675d36f290790b7b6e8b4d
melissard02 <melissa.rd1258@gmail.com>
2025-07-01 12:06:38 -0600

    EternalGoal class implemented into Menu

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,13 @@
+// EternalGoal.cs
+
+public class EternalGoal
+{
+    public EternalGoal()
+    { }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine("Test Line. Press enter to return to the menu");
+        Console.ReadLine();
+    }
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -18,20 +18,20 @@ class Program
                 SimpleGoal simpleGoal = new SimpleGoal();
                 simpleGoal.DisplayPrompt();
             }
-            // else if (choice == 2)
-            // {
-            //     var activity = new Listing("Listing", "Write out your answers.", 0);
-            //     activity.RunActivity();
-            // }
+            else if (choice == 2)
+            {
+                EternalGoal eternalGoal = new EternalGoal();
+                eternalGoal.DisplayPrompt();
+            }
             // else if (choice == 3)
             // {
             //     var activity = new Reflection("Reflection", "Think about these questions.", 0);
             //     activity.RunActivity();
             // }
-            // else if (choice == 4)
-            // {
-            //     running = false;
-            // }
+            else if (choice == 4)
+            {
+                running = false;
+            }
             else
             {
                 Console.WriteLine("Enter a correct value");
